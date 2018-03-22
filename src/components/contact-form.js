0cf3d268d29a81e9d3f4446195d6beb2eddf1b59
saaderana <saaderana@gmail.com>
2018-03-22 14:19:20 -0700

    readded redirect

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -7,7 +7,7 @@ import {updateForm} from '../actions/forms';
 import {checkForm} from '../actions/forms';
 import Input from './input';
 import {required, nonEmpty, email} from '../validators';
-import {Link} from 'react-router-dom'; 
+import {Link, Redirect} from 'react-router-dom'; 
 
 //import styled from 'styled-components';
 //why doesn't above work?
@@ -92,12 +92,12 @@ export class ContactForm extends React.Component {
 
           <div className = "container">
            
-            {/*}           
+                       
              { this.props.isFormCreated &&
                 <Redirect to="/dashboard" />
                 
            }
-            {*/}
+            
            
                <div className = "form-link">
                     <Link to= "/Dashboard" className = "dashboard-link">Go to Dashboard</Link>
@@ -353,7 +353,7 @@ export class ContactForm extends React.Component {
 
 const mapStateToProps = state => {
 
-    //const {currentUser} = state.auth;
+    const {currentUser} = state.auth;
     return {
         username: state.auth.currentUser.username,
         email: state.auth.currentUser.email,

b076d9681ef5c37aa7ac876324b198d25de10f29
saaderana <saaderana@gmail.com>
2018-03-26 10:31:00 -0700

    site description

--- src/components/help.js
+++ src/components/help.js
@@ -10,6 +10,7 @@ export function Help(props) {
     return (
         <div className="home">
             <h2 className="register">Login or Register to get started!</h2>
+            <p className="register">This site allows you to fill and view your version of Dr. Rana's form anywhere with a computer! Just press the Dr. Rana Form button to fill the form after you login. Need to update something? Just press the Dr. Rana Form button again! Your previous answers will auto-fill and you can make any changes you need to and re-submit!</p>
             <Link className = "loginLink" to="/">Login</Link>
             <Link className = "register" to="/register">Register</Link>
         </div>
